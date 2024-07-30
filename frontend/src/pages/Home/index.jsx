026dd3de08eff73aa4459b7b9af121bc07915d17
mucahitcetin <mucahitcetin01@gmail.com>
2024-07-30 23:44:56 +0300

    first commit

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import Container from "../../components/Container";
+
+const Home = () => {
+  return (
+    <Container designs="my-5">
+      <Hero />
+    </Container>
+  );
+};
+
+export default Home;
